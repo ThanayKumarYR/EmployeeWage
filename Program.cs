5fa0bbb639b09cc04cfc6d30f7400fb8c48bb557
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-19 09:06:53 +0530

    intialling the repository

--- Program.cs
+++ Program.cs
@@ -10,18 +10,7 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
-            Random random = new Random();
-
-            int empCheck = random.Next(0,2);
-            if (empCheck == IS_FULL_TIME)
-            {
-                Console.WriteLine("Employee is Preaent");
-            }
-            else
-            { 
-                Console.WriteLine("Employee is not Present");
-            }
+            Console.WriteLine("Welcome to Employee Wage !");
         }
     }
 }
